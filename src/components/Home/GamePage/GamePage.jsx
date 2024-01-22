e1835b9b89981378194305cca4ebda3b18e297ea
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-01-23 01:12:39 +0530

    logic added to the GamePage component

--- src/components/Home/GamePage/GamePage.jsx
+++ src/components/Home/GamePage/GamePage.jsx
@@ -1,39 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Rules from '../../Rules/Rules'
 
 const GamePage = () => {
+    const arrNumber = [1,2,3,4,5,6]
+
+    const [selectNumber, setSelectNumber] = useState("")
+    const [currentDice, setCurrentDice] = useState(1)
+    const [score, setScore] = useState(0)
+
+
+   
+      const generateRandomNumber = (min, max) => {
+        return (
+            Math.floor(Math.random() * (max-min) + min)
+        )
+    }
+
+    const roleDice = ()=>{
+        const randomNumber = generateRandomNumber(1,7);
+setCurrentDice((prev)=> randomNumber)
+
+if(selectNumber === randomNumber)
+    {
+        setScore((prev)=>prev+randomNumber)
+    }
+    else{
+        setScore((prev)=>prev-2)
+    }
+}
+
+
   return (
     <div className="px-5 py-5">
 
     <div className="flex flex-col justify-between sm:flex-row">
         <div className="flex flex-col justify-center items-center m-2">
             <h1 className="px-2 py-2 font-bold text-7xl">
-                0
+                {score}
             </h1>
-            <p className="font-bold px-2">
+            <p 
+             className="font-bold px-2">
                 Total Score
             </p>
         </div>
 
         <div className="flex flex-col m-2">
+
             <div className="px-2 py-2 flex flex-row justify-center items-center">
-                <div className="h-8 w-8 border-2 border-black mx-2 my-2 flex justify-center items-center">
-                    1
-                </div>
-                <div className="h-8 w-8 border-2 border-black mx-2 my-2 flex justify-center items-center">
-                    2
-                </div>
-                <div className="h-8 w-8 border-2 border-black mx-2 my-2 flex justify-center items-center">
-                    3
-                </div> <div className="h-8 w-8 border-2 border-black mx-2 my-2 flex justify-center items-center">
-                    4
-                </div>
-                <div className="h-8 w-8 border-2 border-black mx-2 my-2 flex justify-center items-center">
-                    5
-                </div>
-                <div className="h-8 w-8 border-2 border-black mx-2 my-2 flex justify-center items-center">
-                    6
-                </div>
+            {arrNumber.map((value,index)=>(
+                 <div
+                key={index}
+                onClick={() => setSelectNumber(value)}
+                className={`cursor-pointer h-8 w-8 border-2 border-black mx-2 my-2 flex justify-center items-center ${
+                  value === selectNumber ? 'bg-black text-white' : ''
+                }`}>
+                 {value}
+             </div>
+            ))}
+                
             </div>
             <p className="font-bold px-2 flex justify-center sm:justify-end">
                 Select Number
@@ -42,8 +66,12 @@ const GamePage = () => {
     </div>
 
     <div className="mx-5 my-10 flex flex-col">
-        <div className="mx-2 my-2 flex justify-center items-center">
-            <img className="w-48" src="./images/dice/dice_1.png" />
+        <div 
+        className="mx-2 my-2 flex justify-center items-center">
+
+            <img
+            onClick={roleDice} 
+            className="w-48 cursor-pointer" src={`./images/dice/dice_${currentDice}.png`} />
         </div>
 
         <div className="flex flex-col justify-center items-center">
